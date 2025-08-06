64a40bcda9daed247ef7ec0f030a0f654c48d382
Susan Swinburne <susan.badel@opencastsoftware.com>
2025-08-06 09:16:59 +0100

    VEI-357 added scenarios and placeholders for amend EU VAT section

--- src/test/scala/uk/gov/hmrc/ui/specs/AmendRegistrationSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/AmendRegistrationSpec.scala
@@ -25,14 +25,63 @@ class AmendRegistrationSpec extends BaseSpec {
 
   Feature("Amend registration journeys") {
 
-    Scenario("Intermediary can access the amend registration journey") {
+    Scenario("Intermediary can amend their registration - changing answers from no to yes") {
 
       Given("the intermediary accesses the amend journey within IOSS Intermediary Registration Service")
       auth.goToAuthorityWizard()
       auth.loginUsingAuthorityWizard("100000001", "Organisation", "vatAndIossInt", "amend")
       registration.checkJourneyUrl("change-your-registration")
 
+      When("the intermediary clicks change for Fixed establishments in other countries")
+      registration.selectChangeOrRemoveLink(
+        "eu-fixed-establishment\\?waypoints\\=change-your-registration"
+      )
+
+      And("the intermediary selects yes on the eu-fixed-establishment page")
+      registration.checkJourneyUrl("eu-fixed-establishment?waypoints=change-your-registration")
+      registration.answerRadioButton("yes")
+
+      Then("the intermediary can add tax details for one EU country")
+      registration.checkJourneyUrl("eu-tax/1?waypoints=change-your-registration")
+      registration.selectCountry("Denmark")
+      registration.checkJourneyUrl("eu-fixed-establishment-address/1?waypoints=change-your-registration")
+      registration.enterFETradingName("Company Name")
+      registration.enterFixedEstablishmentAddress("1 Street Name", "", "Town", "", "")
+      registration.checkJourneyUrl("registration-tax-type/1?waypoints=change-your-registration")
+      registration.selectRegistrationType("vat number")
+      registration.checkJourneyUrl("eu-vat-number/1?waypoints=change-your-registration")
+      registration.enterAnswer("DK12345678")
+      registration.checkJourneyUrl("check-tax-details/1?waypoints=change-your-registration")
+      registration.continue()
+      registration.checkJourneyUrl("add-tax-details?waypoints=change-your-registration")
+
+      And("the intermediary can add tax details for another EU country")
+      registration.answerRadioButton("yes")
+      registration.checkJourneyUrl("eu-tax/2?waypoints=change-your-registration")
+      registration.selectCountry("Slovakia")
+      registration.checkJourneyUrl("eu-fixed-establishment-address/2?waypoints=change-your-registration")
+      registration.enterFETradingName("Company Name")
+      registration.enterFixedEstablishmentAddress("1 Street Name", "", "Town", "", "")
+      registration.checkJourneyUrl("registration-tax-type/2?waypoints=change-your-registration")
+      registration.selectRegistrationType("tax id number")
+      registration.checkJourneyUrl("eu-tax-identification-number/2?waypoints=change-your-registration")
+      registration.enterAnswer("12345A")
+      registration.checkJourneyUrl("check-tax-details/2?waypoints=change-your-registration")
+      registration.continue()
+      registration.checkJourneyUrl("add-tax-details?waypoints=change-your-registration")
+
+      And("the intermediary can submit their amended registration")
+      registration.answerRadioButton("no")
+      registration.checkJourneyUrl("change-your-registration")
+      registration.submit()
+      registration.checkJourneyUrl("successful-amend")
     }
+
+//    Extra scenarios to add when view registration API is implemented
+    // Scenario("Intermediary removes all answers for a section when amending their registration - changing answers from yes to no")
+//        - add for EU tax details
+    // Scenario("Intermediary can amend and remove individual details from their existing registration answers")
+//          - add for EU tax details
 
     Scenario("Intermediary can cancel their amended registration") {
 
